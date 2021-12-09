cac30c651b58b691ed05729b6777af61842cffbc
HarshTripathi12 <95767789+HarshTripathi12@users.noreply.github.com>
2021-12-09 22:28:53 +0530

    Add files via upload

---
+++ Area of Trapezium.c
@@ -0,0 +1,11 @@
+// To print Area of Trapezium
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+        float base,height,area;
+        scanf("%f%f",&base,&height);
+        area=(base+base)/2*height;
+       printf("%f",area);
+    return 0;
+}
