cdd3596c8749914eb6797a8bd006c50b76dd6107
HarshTripathi12 <95767789+HarshTripathi12@users.noreply.github.com>
2021-12-09 22:53:37 +0530

    Add files via upload

---
+++ To print Volume of sphere.c
@@ -0,0 +1,11 @@
+// To print Volume of sphere
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    float r,area;
+    scanf("%f",&r);
+    area=(4.0/3.0)*3.14*r*r*r;
+    printf("%f",area);
+    return 0;
+}
