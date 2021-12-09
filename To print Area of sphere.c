29e7fb94d02831479d03d432b1eb4716274c12a9
HarshTripathi12 <95767789+HarshTripathi12@users.noreply.github.com>
2021-12-09 22:47:47 +0530

    Add files via upload

---
+++ To print Area of sphere.c
@@ -0,0 +1,11 @@
+// To print Area of sphere
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    float r,area;
+    scanf("%f",&r);
+    area=4*3.14*r*r;
+    printf("%f",area);
+    return 0;
+}
