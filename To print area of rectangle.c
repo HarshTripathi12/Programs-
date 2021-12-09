2cf4636de88e47eb05aa413ae09d5d88ed9c4b97
HarshTripathi12 <95767789+HarshTripathi12@users.noreply.github.com>
2021-12-09 22:57:55 +0530

    Add files via upload

---
+++ To print area of rectangle.c
@@ -0,0 +1,11 @@
+// To print area of rectangle
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    float l,b,area;
+    scanf("%f%f",&l,&b);
+    area=l*b;
+    printf("%f",area);
+    return 0;
+}
