55f03a5ac0aa778754793d1a826ee67e1c1c3c0f
HarshTripathi12 <95767789+HarshTripathi12@users.noreply.github.com>
2022-01-16 22:35:24 +0530

    Add files via upload

---
+++ noargumentwithreturn.c
@@ -0,0 +1,42 @@
+/*#include<stdio.h>
+int main ()
+{
+    int x;
+    scanf("%d",&x);
+    if (x>0)
+    {
+        printf("positive");
+    }
+    else if (x<0)
+    {
+        printf("negative");
+    }
+    else
+    {
+        printf("zero");
+    }
+    return 0;
+}*/
+#include<stdio.h>
+int oddeven ()
+{
+int x;
+scanf("%d",&x);
+}
+int main ()
+{
+    int x;
+       if (x>0)
+    {
+        printf("positive");
+    }
+    else if (x<0)
+    {
+        printf("negative");
+    }
+    else
+    {
+        printf("zero");
+    }
+    return 0;
+}
